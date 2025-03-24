71d14117065d66086530b418ae94294908ca9d34
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-03-24 11:28:22 +0200

    Create exercise.js

---
+++ exercise.js
@@ -0,0 +1,7 @@
+const a = 10;
+const b = 5;
+const sum = a+b;
+const pro = a*b;
+const diff = a-b;
+const quo = a/b;
+console.log(`the sum is equal ${sum}\nthe product is equal ${pro}\nthe difference is equal ${diff}\nthe quotient is equal ${quo}`);
